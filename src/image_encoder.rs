daae3563dfec3b0d6149fbe5afe1c7635e0832c6
hgallagher1993 <hgallagher1993@gmail.com>
2019-03-06 12:02:22 -0500

    Add method to convert message to vec of bits

--- src/image_encoder.rs
+++ src/image_encoder.rs
@@ -1,10 +1,12 @@
 use image::{RgbImage, ImageBuffer};
+use bitreader::BitReader;
 
 #[derive(Debug)]
 pub struct CoverImage {
     message: &'static str,
     output_image_path: &'static str,
-    cover_image: RgbImage
+    cover_image: RgbImage,
+    message_as_bits: Vec<u8>
 }
 
 impl CoverImage {
@@ -12,7 +14,8 @@ impl CoverImage {
         CoverImage {
             message: "",
             output_image_path: "",
-            cover_image: ImageBuffer::new(0, 0)
+            cover_image: ImageBuffer::new(0, 0),
+            message_as_bits: vec![]
         }
     }
 
@@ -37,6 +40,24 @@ impl CoverImage {
     }
 
     pub fn encode(&mut self) {
-
+        self.get_message_as_bits();
     }
+
+    fn get_message_as_bits(&mut self) {
+        let mut reader = BitReader::new(&self.message.as_bytes());
+
+        for _ in 0..self.message.len() * 8 {
+            self.message_as_bits.push(reader.read_u8(1).unwrap());
+        }
+    }
+}
+
+#[test]
+fn get_message_as_bits_test() {
+    let test_bits = vec![0, 1, 1, 0, 1, 0, 0, 0];
+    let mut cover_image = CoverImage::new().set_message("h");
+
+    cover_image.get_message_as_bits();
+
+    assert_eq!(cover_image.message_as_bits, test_bits);
 }
