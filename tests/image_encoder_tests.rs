cc23b22be97b0dbcfe1db3effa51a2dc75d67010
hgallagher1993 <hgallagher1993@gmail.com>
2019-03-06 11:43:38 -0500

    This test shouldn't have been commited in the first place

--- tests/image_encoder_tests.rs
+++ tests/image_encoder_tests.rs
@@ -2,10 +2,4 @@ extern crate rsteglib;
 
 use rsteglib::image_encoder::CoverImage;
 
-#[test]
-fn create() {
-    let cover_image = CoverImage::new()
-        .set_cover_image("/home/hugh/Pictures/static.politico.com.jpeg")
-        .set_message("whatever")
-        .set_output_image_path("hdsd");
-}
+
