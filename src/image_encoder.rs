857756ffc304b6696beea5a77040c0ce1a16da7f
hgallagher1993 <hgallagher1993@gmail.com>
2019-03-06 14:42:38 -0500

    Add funcionality to tile image into 8x8 blocks

--- src/image_encoder.rs
+++ src/image_encoder.rs
@@ -1,4 +1,4 @@
-use image::{RgbImage, ImageBuffer};
+use image::{ImageBuffer, RgbImage};
 use bitreader::BitReader;
 
 #[derive(Debug)]
@@ -39,16 +39,41 @@ impl CoverImage {
         self
     }
 
-    pub fn encode(&mut self) {
-        self.get_message_as_bits();
-    }
-
     fn get_message_as_bits(&mut self) {
         let mut reader = BitReader::new(&self.message.as_bytes());
 
         for _ in 0..self.message.len() * 8 {
             self.message_as_bits.push(reader.read_u8(1).unwrap());
         }
+    }
+
+    fn tile_image(&mut self) -> Vec<u8> {
+        let (width, height) = self.cover_image.dimensions();
+
+        let mut tiles = Vec::new();
+        let mut count = 0;
+
+        for row_index in 0..(height / 8) as u32 {
+            for col_index in 0..(width / 8) as u32 {
+                for channel in 0..3 {
+                    if count == self.message_as_bits.len() {
+                        break
+                    }
+
+                    for row in 0..8 {
+                        for column in 0..8 {
+                            let pixel = self.cover_image.get_pixel(column + (col_index * 8),
+                                                                      row + (row_index * 8));
+
+                            tiles.push(pixel.data[channel]);
+                        }
+                    }
+                    count += 1;
+                }
+            }
+        }
+
+        tiles
     }
 }
 
@@ -60,4 +85,43 @@ fn get_message_as_bits_test() {
     cover_image.get_message_as_bits();
 
     assert_eq!(cover_image.message_as_bits, test_bits);
+}
+
+#[test]
+fn tile_image_length_test() {
+    let mut cover_image = CoverImage::new().set_message("h")
+        .set_cover_image("src/testing.jpg");
+
+    // 64 = length of 1 tile, message.len() * 8 = number of tiles needed; 1 tile per bit
+    let length = 64 * cover_image.message.len() * 8;
+
+    cover_image.get_message_as_bits();
+
+    let tile_vec_length = cover_image.tile_image().len();
+
+    assert_eq!(length, tile_vec_length);
+}
+
+#[test]
+fn test_random_pixel_in_tile() {
+    let mut cover_image = CoverImage::new().set_message("h")
+        .set_cover_image("src/testing.jpg");
+
+    cover_image.get_message_as_bits();
+
+    let tile_vec = cover_image.tile_image();
+
+    let pixel = cover_image.cover_image.get_pixel(8, 8);
+
+    let r_pixel = pixel.data[0];
+    let g_pixel = pixel.data[1];
+    let b_pixel = pixel.data[2];
+
+    let r_pixel_from_tile = tile_vec[63];
+    let g_pixel_from_tile = tile_vec[127];
+    let b_pixel_from_tile = tile_vec[191];
+
+    assert_eq!(r_pixel, r_pixel_from_tile);
+    assert_eq!(g_pixel, g_pixel_from_tile);
+    assert_eq!(b_pixel, b_pixel_from_tile);
 }
